864f2bc3e734a38ca1dd6aa43b2046d896fb2749
Mathéo DELAUNAY <dseonay@gmail.com>
2025-10-26 16:23:55 +0100

    feat: add dockerFile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
             style: {
               background: 'rgba(255, 255, 255, 0.1)',
               backdropFilter: 'blur(10px)',
-              color: '#fff',
+              color: '#000',
               border: '1px solid rgba(255, 255, 255, 0.2)',
             },
           }}
